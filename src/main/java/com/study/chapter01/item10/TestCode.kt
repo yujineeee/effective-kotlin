6e92732680e15f30200fe007b1200e09604e6966
장유진/전시개발팀 <yujin.jang@musinsa.com>
2024-11-13 02:58:03 +0900

    chapter01 > item10: 단위 테스트를 만들어라

---
+++ src/main/java/com/study/chapter01/item10/TestCode.kt
@@ -0,0 +1,10 @@
+package com.study.chapter01.item10
+//
+//@Test
+//fun 'fib works correctly for the first 5 positions'() {
+//    assertEquals(1, fib(0))
+//    assertEquals(1, fib(1))
+//    assertEquals(2, fib(2))
+//    assertEquals(3, fib(3))
+//    assertEquals(5, fib(4))
+//}
